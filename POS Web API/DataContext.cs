8eea390c0a1661bfa0e72bcbf9ff1db09e16740a
Ali Najafi <Ali Najafi@DESKTOP-O65B9UK>
2024-07-12 22:15:45 +0200

    #

--- POS Web API/DataContext.cs
+++ POS Web API/DataContext.cs
@@ -4,24 +4,24 @@
 
 
 public class DataContext : DbContext
+{
+
+
+
+
+
+
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
 
-
-
-
-
-    
-    public DataContext(DbContextOptions<DataContext> options) : base(options)
-        {
-
-        }
-
-        public DbSet<Product> Product { get; set; }
-
-        public DbSet<Transaction> Transaction_ { get; set; }
-
-        public DbSet<TransactionDetail> TransactionDetail { get; set; }
-
-     
     }
+
+    public DbSet<Product> Product { get; set; }
+
+    public DbSet<Transaction> Transaction_ { get; set; }
+
+    public DbSet<TransactionDetail> TransactionDetail { get; set; }
+
+
+}
 
