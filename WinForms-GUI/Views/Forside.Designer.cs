708df056f5d7742c7580d41f20d648f8b3c261de
Ali Najafi <Ali Najafi@DESKTOP-O65B9UK>
2024-08-11 09:10:54 +0200

    #

--- WinForms-GUI/Views/Forside.Designer.cs
+++ WinForms-GUI/Views/Forside.Designer.cs
@@ -1099,6 +1099,7 @@
             this.button1.TabIndex = 307;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnRunPython_Click);
             // 
             // Forside
             // 
